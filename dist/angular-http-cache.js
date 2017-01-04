1563f3d8761dc0515287813b0d9ddf80673682f8
Nick Gerner <hello@njgerner.com>
2017-01-03 21:32:04 -0500

    v1.2.1

--- dist/angular-http-cache.js
+++ dist/angular-http-cache.js
@@ -240,7 +240,6 @@ angular.module('angular-http-cache', ['angular-local-db']).service('$httpCache',
      */
     httpCache.prototype.getCollection = function () {
         return this._collection;
-        return this;
     };
 
     /**
@@ -265,6 +264,7 @@ angular.module('angular-http-cache', ['angular-local-db']).service('$httpCache',
      */
     httpCache.prototype.setDomain = function (domain) {
         this._domain = domain;
+        return this;
     };
 
     /**
